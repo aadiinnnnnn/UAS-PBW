c03c0d6b00aa050e61667eeb5dc64f77ea147d96
aadiinnnnnn <adinoaza@gmail.com>
2025-05-20 21:25:09 +0700

    first commit

---
+++ admin/login.php
@@ -0,0 +1,34 @@
+<?php include 'koneksi.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/vendor/bootstrap-5.0.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/vendor/boxicons-2.1.4/css/boxicons.min.css">
+</head>
+<style>
+.main {
+    height: 100vh;
+}
+
+.login-box {
+    width: 500px;
+    height: 300px;
+    border: solid 1px;
+}
+</style>
+
+<body>
+    <div class="main d-flex justify-content-center align-items-center">
+        <div class="login-box"></div>
+    </div>
+    <script src="/vendor/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
+    <script src="/vendor/jquery-3.7.1.min.js"></script>
+</body>
+
+</html>
