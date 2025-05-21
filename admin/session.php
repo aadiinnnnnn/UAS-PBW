7e0b72bfb1c36fff5c38c252a1debe15c76ca624
aadiinnnnnn <adinoaza@gmail.com>
2025-05-21 17:54:33 +0700

    update-code

--- admin/session.php
+++ admin/session.php
@@ -1,6 +1,7 @@
 <?php 
-    session_start();
-    if ($_SESSION ['login']==false) {
-        header('location: login.php');
-    }
+session_start();
+if (!isset($_SESSION['login']) || $_SESSION['login'] == false) {
+    header('Location: login.php');
+    exit;
+}
 ?>
